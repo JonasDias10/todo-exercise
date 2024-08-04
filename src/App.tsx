b200a7448f5a00cc3d5aac0e2501c13c2616bd86
Jonas Dias <zdias1413@gmail.com>
2024-08-03 23:41:53 -0300

    feat: setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+    </BrowserRouter>
+  );
+}
